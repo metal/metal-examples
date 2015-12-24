eca8146e779802377c5040b11695775e70de9199
Maira Bello <maira.araujo@liferay.com>
2015-12-24 13:21:24 -0300

    Adds an example for building metal components with JSPM

--- custom-build/no-soy/test/MousePosition.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-import MousePosition from '../src/MousePosition';
-
-describe('MousePosition', function() {
-	it('should be tested', function() {
-		assert.fail('No tests for this module yet.');
-	});
-});
