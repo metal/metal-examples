01bcc7f604ff5d73c5641d8785b54a583c9ec939
Bence Varga <vbence86@gmail.com>
2017-08-25 11:26:07 +0200

    remove advanced examples from boilerplate folder

--- boilerplates/webpack-with-soy/test/Modal.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-import Modal from '../src/index';
-
-describe('Modal', function() {
-	it('should be tested', function() {
-		assert.fail('No tests for this module yet.');
-	});
-});
